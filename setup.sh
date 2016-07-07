0d86a8acfdef281581509ab29b88d2ced41b0feb
Junior Moura <natmourajr@gmail.com>
2016-07-07 14:47:51 -0300

    Create a new struct to read data

---
+++ setup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash 
+
+# SonarAnalysis Setup Script
+#
+# Author: natmourajr@gmail.com
+#
+
+# Env Variables
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    # Ubuntu
+    export SONAR_WORKSPACE=/home/natmourajr/Workspace/Doutorado/SonarAnalysis
+    export INPUTDATAPATH=/home/natmourajr/Public/Marinha/Data
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # Mac OSX
+    export SONAR_WORKSPACE=
+    export INPUTDATAPATH=
+fi
+
+export OUTPUTDATAPATH=$SONAR_WORKSPACE/Results
+
+export MY_PATH=$PWD
+
+# Folder Configuration
+if [ -d "$OUTPUTDATAPATH" ]; then
+    read -e -p "Folder $OUTPUTDATAPATH exist, Do you want to erase it? [Y,n] " yn_erase
+    if [ "$yn_erase" = "Y" ]; then
+        echo "creating OUTPUTDATAPATH struct"
+        rm -rf $OUTPUTDATAPATH
+        mkdir $OUTPUTDATAPATH
+        cd $SONAR_WORKSPACE/Packages
+        for i in $(ls -d */); do mkdir $OUTPUTDATAPATH/${i%%/}; done
+        cd $MY_PATH
+    fi
+else
+    echo "OUTPUTDATAPATH: $OUTPUTDATAPATH doesnt exists"
+    echo "creating OUTPUTDATAPATH struct"
+    rm -rf $OUTPUTDATAPATH
+    mkdir $OUTPUTDATAPATH
+    cd $SONAR_WORKSPACE/Packages
+    for i in $(ls -d */); do mkdir $OUTPUTDATAPATH/${i%%/}; done
+    cd $MY_PATH
+fi
+
+if [ -d "$OUTPUTDATAPATH/DataHandler" ]; then
+	rm -rf $OUTPUTDATAPATH/DataHandler
+fi
