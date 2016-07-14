9d712cbb1990880379982adc5321d2e5a7878259
Junior Moura <natmourajr@gmail.com>
2016-07-14 09:24:14 -0300

    new version of PCASingleClassSVM

---
+++ Functions/StatFunctions.py
@@ -0,0 +1,24 @@
+""" 
+  This file contents stat functions
+"""
+
+import numpy as np
+
+def KLDiv(p,q):
+	if len(p) != len(q):
+		print 'Different dimensions'
+		return -1
+	
+	kl_values = []
+	for i in range(len(p)):
+		if p[i] == 0 or q[i] == 0 : 
+			kl_values = np.append(kl_values,0)
+		else:
+			kl_value = np.absolute(p[i])*np.log10(np.absolute(p[i])/np.absolute(q[i]))
+			if np.isnan(kl_value):
+				kl_values = np.append(kl_values,0)
+			else:
+				kl_values = np.append(kl_values,kl_value)
+			#print "KLDiv: p= %f, q=%f, kl_div= %f"%(p[i],q[i],kl_values[i])
+			
+	return [np.sum(kl_values),kl_values]
