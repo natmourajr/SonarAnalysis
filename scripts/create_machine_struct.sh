39617aba1d7323ac238fbbac9a153e8739949bc1
Junior Moura <natmourajr@gmail.com>
2016-09-21 20:07:04 -0300

    configuration in script files

--- scripts/create_machine_struct.sh
+++ scripts/create_machine_struct.sh
@@ -2,7 +2,7 @@
 
 # function for create a port forward connection
 
-#usage ./create_machine_struct.sh
+#usage source create_machine_struct.sh
 
 # Preprocessing 
 ./create_connection.sh 8777 cessy 8777
@@ -11,7 +11,9 @@
 ./create_connection.sh 8778 cessy 8778
 
 # Stationarity Analysis
-#./create_connection.sh 8779 cessy 8779
+#./create_connection.sh 8779 8779
 
 # Classification
 ./create_connection.sh 8779 cessy 8779
+
+
